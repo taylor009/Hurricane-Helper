6aa649ee6b4874f68b0b48617db5011432987616
1337erik <erikpwhite@gmail.com>
2019-10-05 11:28:18 -0400

    adding barebones ui

---
+++ client/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+
+    <navbar />
+    <div id="nav">
+
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+
+      <b-container>
+
+        <b-row>
+
+          <b-col>
+
+            <div id="containerDiv" style="width: 320px; min-width: 200px; height: 465px; min-height: 400px; ">
+
+              <!--Amazon CCP will go here-->
+            </div>
+          </b-col>
+          <b-col>
+
+            <router-view/>
+          </b-col>
+        </b-row>
+      </b-container>
+  </div>
+</template>
+
+<script>
+
+  import Navbar from './components/Navbar';
+
+  export default {
+
+    data: () => ({
+
+      ccpUrl: "https://pbt-hackathon.awsapps.com/connect/ccp#", //replace with the CCP URL for your Amazon Connect instance
+    }),
+    mounted(){
+
+      connect.core.initCCP( containerDiv, {
+
+        ccpUrl     : this.ccpUrl,
+        loginPopup : true,
+        softphone  : {
+
+            allowFramedSoftphone: true
+        }
+      });
+    },
+    components : {
+
+      Navbar
+    }
+  }
+</script>
+
+<style lang="scss">
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-nav>
+
+        <b-nav-item active>Active</b-nav-item>
+        <b-nav-item>Link</b-nav-item>
+        <b-nav-item>Another Link</b-nav-item>
+        <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
