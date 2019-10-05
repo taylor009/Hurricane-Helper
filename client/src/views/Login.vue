c6009d34deb44f72df18ddefdb5724da58bc06c8
Michael Roth <michael.franklin.roth@gmail.com>
2019-10-05 16:21:42 -0400

    login.vue now has LoginForm.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,15 +1,49 @@
 <template>
 
-  <div>
+    <b-container>
 
-      <h1>This logs you in lmao</h1>
-  </div>
+        <b-row class="h-100 d-flex align-items-center justify-content-center">
+          
+            <b-col class="">
+
+                <form-example />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 
+import FormExample from '../components/Forms/LoginForm';
+
     export default {
 
+        data: () => ({
+
+            mounted : false,
+            ccpUrl: "https://pbt-hackathon.awsapps.com/connect/ccp#", //replace with the CCP URL for your Amazon Connect instance
+        }),
+        mounted(){
+
+            if( !this.mounted ){
+
+                connect.core.initCCP( containerDiv, {
+
+                    ccpUrl     : this.ccpUrl,
+                    loginPopup : true,
+                    softphone  : {
+
+                        allowFramedSoftphone: true
+                    }
+                });
+
+                this.mounted = true;
+            }
+        },
+        components : {
+
+            FormExample
+        }
     }
 </script>
 
