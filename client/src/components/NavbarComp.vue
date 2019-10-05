fdb8e7d5efa08208816520ac851de5b87e4b8ee8
Michael Roth <michael.franklin.roth@gmail.com>
2019-10-05 13:48:45 -0400

    init push

--- client/src/components/NavbarComp.vue
+++ client/src/components/NavbarComp.vue
@@ -1,36 +1,27 @@
 <template>
-
-    <b-nav id="nav" class="d-flex align-items-center">
-
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/about">About</b-nav-item>
+  <b-nav id="nav" class="d-flex align-items-center">
+    <b-nav-item to="/">Home</b-nav-item>
+    <b-nav-item to="/about">About</b-nav-item>
   </b-nav>
 </template>
 
 <script>
-
-    export default {
-
-    }
+export default {};
 </script>
 
 <style scoped lang="scss">
+#nav {
+  position: fixed;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 2px 5px 0px #ccc;
 
-    #nav {
-
-        position: fixed;
-        width: 100%;
-        background-color: white;
-        box-shadow: 0px 2px 5px 0px #ccc;
-
-        a {
-
-            font-weight: bold;
-            color: #2c3e50;
-            &.router-link-exact-active {
-
-                color: #42b983;
-            }
-        }
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
+}
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,26 @@
 <template>
+  <div>
+    <div class="home">
+      <div class="home-top-half">
+        <h1>This is the Home Page</h1>
+      </div>
 
-  <div class="home">
-
-    <h1>This is the Home Page</h1>
+      <div class="home-bottom-half"></div>
+    </div>
   </div>
 </template>
 
 <script>
-  // @ is an alias to /src
+// @ is an alias to /src
 
-  export default {
-
-    name: 'home',
-    components: {
-
-    }
-  }
+export default {
+  name: "home",
+  components: {}
+};
 </script>
+
+<style scoped>
+.home {
+  margin: 2.5%;
+}
+</style>
