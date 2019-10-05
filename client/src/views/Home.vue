30521eaf58295cef2240589a9d1c8a3f70ea2235
Michael Roth <michael.franklin.roth@gmail.com>
2019-10-05 13:57:07 -0400

    attempting to adjust height on home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <div class="home">
+    <div class="home d-flex flex-column justify-content-between">
       <div class="home-top-half">
         <h1>This is the Home Page</h1>
       </div>
 
-      <div class="home-bottom-half"></div>
+      <div class="home-bottom-half d-flex justify-content-between">
+        <div class="signup signup-user">
+          <a href>User Signup</a>
+        </div>
+        <div class="signup signup-volunteer">
+          <a href>Volunteer Signup</a>
+        </div>
+        <div class="signup signup-organization">
+          <a href>Organization Signup</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +32,7 @@ export default {
 <style scoped>
 .home {
   margin: 2.5%;
+  /* height: 500px; */
+  height: 100%;
 }
 </style>
