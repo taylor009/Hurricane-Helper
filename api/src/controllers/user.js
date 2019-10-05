091753ec6820840df46fe746b72b7397732ddd0f
taylor009 <taylor@baremetalcloud.com>
2019-10-05 16:23:24 -0400

    created create user controller function.

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,7 +1,34 @@
 'use strict';
+const argon2 = require('argon2');
+const logger = require('../config/logger');
 
-exports.createUser = (req, res, next) =>
+const User = require('../models/user');
+
+exports.createUser = async (req, res, next) =>
 {
-  res.send('Hello World');
+  try{
+    const hash = await argon2.hash(req.body.password, {hashLength: 40}).then(hash =>
+    {
+      const user = new User({
+        phoneNumber: req.body.phoneNumber,
+        password: hash
+      });
+      user.save().then(result =>
+      {
+        res.status(201).json({
+          message: 'User created!',
+          result: result
+        });
+      }).catch(error =>
+      {
+        logger.error(`Error creating a User: ${error.message}`)
+      })
+    });
+    hash()
+  }
+  catch(error)
+  {
+    logger.error(`Error during creation of user ${error.message}`);
+  }
   next();
 };
