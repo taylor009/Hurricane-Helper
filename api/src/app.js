a6e3575c991092f1dd628f82535e6f1f0d256db4
taylor009 <taylor@baremetalcloud.com>
2019-10-05 21:14:19 -0400

    cleaned up app file

--- api/src/app.js
+++ api/src/app.js
@@ -10,18 +10,18 @@ const bodyParser = require('body-parser');
 const logger     = require('./config/logger');
 
 // Imported Route Files
-const loginRoutes = require('./routes/login');
-const directoryRoutes = require('./routes/directory');
-const userRoutes  = require('./routes/users');
+const loginRoutes       = require('./routes/login');
+const directoryRoutes   = require('./routes/directory');
+const userRoutes        = require('./routes/users');
 const directoriesRoutes = require('./routes/directories');
-const smsRoutes = require('./routes/sms');
-const sosRoutes = require('./routes/sos');
-const okayRoutes = require('./routes/okay');
-const signUpRoutes = require('./routes/signup');
+const smsRoutes         = require('./routes/sms');
+const sosRoutes         = require('./routes/sos');
+const okayRoutes        = require('./routes/okay');
+const signUpRoutes      = require('./routes/signup');
 
 const myCredentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'IDENTITY_POOL_ID'});
 let myConfig        = new AWS.Config({
-    credentials: myCredentials, region     : 'us-east-1',
+    credentials: myCredentials, region: 'us-east-1',
 });
 
 // Set the region
@@ -30,11 +30,12 @@ AWS.config.update({region: "us-east-1"});
 // Create the DynamoDB service object
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-const createDynamooseInstance = () => {
+const createDynamooseInstance = () =>
+{
     dynamoose.AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        accessKeyId    : process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: 'us-east-1'
+        region         : 'us-east-1'
     });
     dynamoose.setDDB(ddb); // This defaults to "http://localhost:8000"
 };
@@ -43,7 +44,8 @@ createDynamooseInstance();
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use((req, res, next) =>
+{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
